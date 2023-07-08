8eafc79b5976630173a293e26b4242d7b77f9ae3
unknown <rockstro@uni-potsdam.de>
2023-07-08 15:02:21 +0200

    adapted reference file for num of participants

--- formatting.py
+++ formatting.py
@@ -6,7 +6,7 @@ from os.path import exists
 
 def display_num_participants():
     # manually write content into sidebar
-    if exists("data/ratings_fit.csv"):
+    if exists("data/knowledge_ents.csv"):
         participants = pd.read_csv("data/knowledge_ents.csv")
         num_users = len(participants)
     else:
