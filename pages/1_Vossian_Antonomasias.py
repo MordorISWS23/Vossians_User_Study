6ea16a591c824e0de128f4ff6abc89d6a3bec1df
unknown <rockstro@uni-potsdam.de>
2023-07-14 19:34:43 +0200

    added selection for final samples

--- pages/1_Vossian_Antonomasias.py
+++ pages/1_Vossian_Antonomasias.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import random
+import numpy as np
 import os
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.add_vertical_space import add_vertical_space
@@ -8,8 +8,10 @@ from formatting import format_sidebar_radio_va
 
 
 class VossianAntonomasiasRater:
-    def __init__(self, ratings_fit, ratings_original, ratings_understand):
-        self.sentences = None
+    def __init__(self, samples, ratings_fit, ratings_original, ratings_understand):
+        self.sentences = list()
+        self.samples = samples
+        self.final_sample = None
         self.ratings_fit = ratings_fit
         self.ratings_original = ratings_original
         self.ratings_understand = ratings_understand
@@ -24,17 +26,45 @@ class VossianAntonomasiasRater:
         st.title("Rate Vossian Antonomasias")
         st.markdown('<p style="font-size: 20px;">Please rate the following sentences with <b>1</b> being the best and '
                     '<b>5</b> being the worst evaluation.</p>', unsafe_allow_html=True)
+        # create final version of data sample, new one for every participant
+        self.sample()
+        print(self.final_sample)
+        alive_dead = {"Angela Merkel": "is",
+                      "Nelson Mandela": "was",
+                      "Bill Gates": "is",
+                      "Alber Einstein": "was",
+                      "Mark Twain": "was",
+                      "Ronald Reagan": "was"
+                      }
+        # generate sentences from entities and domain
+        for i, row in self.final_sample.iterrows():
+            A = str(row['A'])
+            B = str(row['B'])
+            C = str(row['C'])
+            sent = f"{A} {alive_dead[A]} the {B} of {C}"
+            self.sentences.append(sent)
+        self.handle_ratings()
 
-        df_samples = pd.read_csv("data/samples.csv")
-        self.sentences = list(df_samples["VA"])
-        random.shuffle(self.sentences)
-        self.handle_ratings(df_samples)
+    def sample(self):
+        # read csv and start random process
+        df_samples = pd.read_csv(self.samples)
+        # randomly select combination of one A and one model for every unique value of model and A -> 42 sentences
+        random_rows = df_samples.groupby(['A', 'Model']).apply(lambda x: x.sample(n=1)).reset_index(drop=True)
+        # randomly select 3 A's so that every A has a unique method
+        sampled_values = random_rows["A"].drop_duplicates().sample(n=3, replace=False)
+        # reduce dataframe to randomly selected three A's -> 21 sentences
+        reduced_other_df = random_rows[random_rows['A'].isin(sampled_values)]
+        # shuffle dataframe based on column "model"
+        reduced_other_df['Model'] = np.random.permutation(reduced_other_df['Model'].values)
+        reduced_other_df = reduced_other_df.sort_values(by=['A'])
+        reduced_other_df.reset_index(drop=True)
+        self.final_sample = reduced_other_df
 
-    def handle_ratings(self, data):
+    def handle_ratings(self):
         key_fit = 0
         key_original = 0
         key_understand = 0
-        for sent in data["VA"]:
+        for sent in self.sentences:
             key_fit += 1
             key_original += 1
             key_understand += 1
@@ -90,7 +120,7 @@ class VossianAntonomasiasRater:
 
 
 # Create an instance of the VossianAntonomasiasRater class
-rater = VossianAntonomasiasRater(ratings_fit="data/ratings_fit.csv", ratings_original="data/ratings_original.csv",
+rater = VossianAntonomasiasRater(samples="data/gen_sents.csv", ratings_fit="data/ratings_fit.csv", ratings_original="data/ratings_original.csv",
                                  ratings_understand="data/ratings_understand.csv")
 # Call the main function
 rater.main()
