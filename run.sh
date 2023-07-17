ac9cfbaefc29a31123d75e797a8de7b3ccfeb841
Nicolas <nicolaslazzari98@gmail.com>
2023-07-17 10:32:51 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd /opt/vossians
 source /opt/vossians/venv/bin/activate
-streamlit run introduction.py --sever.port=8501 --server.address=127.0.0.1
+streamlit run introduction.py --server.port=8501 --server.address=127.0.0.1
